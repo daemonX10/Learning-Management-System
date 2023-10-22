902f0e781f9ea66189e5a587d4beac4628e2f39a
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-23 00:22:19 +0530

    b

--- server/app.js
+++ server/app.js
@@ -1,25 +1,26 @@
-import cookieParser from "cookie-parser";
-import { express } from "express";
+const  cookieParser = require("cookie-parser");
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cor({
+app.use(cors({
     origin:[process.env.FRONTEND_URL, 'http://localhost:3000'],
-    Credentials: true
+    credentials: true
 }));
 
 app.use(cookieParser());
 
-app.use('ping',(req,res)=>{
-    app.send('pong');
+app.use('/ping',(req,res)=>{
+    res.send('pong');
 })
 
 // 3 route config
 
-app.all("*",()=>{
+app.all("*",(req,res)=>{
     res.status(404).send("OOPS! , 404 Page not found")
 } );
 
-export default app;
+module.exports = app;
