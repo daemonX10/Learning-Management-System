2d1cb27d181c0352a9acd7374aaae0f07f91ad37
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-29 08:38:40 +0530

    hh

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`server is running on port http://localhost:${PORT}`)
