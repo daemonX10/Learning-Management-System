9d6da569e6b528e25f26d4e72424ad3b84725465
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-10-31 02:44:49 +0530

    utils

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,6 +1,19 @@
-const register = (req,res)=>{
+const User = require("../models/user.models");
+
+const register = async (req,res)=>{
     // register logic
+    const { fullname , email , password} = req.body;
+
+    if(!fullname || !email || !password){
+        return next(new AppError("All Field are required ", 400));
+    }
+    const userExists = await User.findOne({email});
     
+    if (userExists){
+        return next( new AppError("Email already exists", 400));
+    }
+
+    const user = User.create();
 }
 
 const login = (req,res)=>{
