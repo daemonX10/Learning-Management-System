d8a7b80e709b128e800e045be2fc920ff13ee210
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-03 16:40:20 +0530

    f

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 mongoose.set('strictQuery',false);
 
-const ConnectToDB =async ()=>{
+const ConnectToDB = async () => {
     try {
-        const { connection }= await mongoose.connect(
-            process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/lms-h');
-        if (connection){
-            console.log(`Connection to MongoDb"${connection}`)
+        const { connection } = await mongoose.connect(
+            process.env.MONGO_URI);
+        if (connection) {
+            console.log(`Connection to MongoDb is successful`);
         }
     } catch (error) {
         console.log(error);
         process.exit(1);
     }
-}
+};
 
 export default ConnectToDB;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import app from './app.js';
 
-import ConnectToDB from './config/dbConnect.js';
+// import ConnectToDB from './config/dbConnect.js';
 
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
-    ConnectToDB
+    
     console.log(`server is running on port http://localhost:${PORT}`)
 })
