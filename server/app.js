3088eb89d1936fc39b861527514af61eaa856f00
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-07 22:25:23 +0530

    Add CORS middleware and server configuration

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 import express from 'express'; 
+import cors from 'cors';
+/* The `cors` middleware is used to enable Cross-Origin Resource Sharing (CORS) in the Express application. CORS is a mechanism that allows resources (e.g., APIs) ona web page to be requested from another domain outside the domain from which theresource originated. */
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
 const app = express();  
 
+app.use(express.json());
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    credentials:true
+}));
+app.use(cookieParser());
 
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
 import app from './app.js';
+
+
+
+const PORT  = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
