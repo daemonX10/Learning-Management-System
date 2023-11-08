017d66c3332077d51256922021232ffcedeee1e5
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-08 18:08:03 +0530

    Add environment variables and update database connection.

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 mongoose.set('strictQuery', false);
 
 const connectToDB = async ()=>{
     try {
         const { connection } = await mongoose.connect(
-            process.env.MONGO_URI || "mongodb://localhost:27017/lms-h",
+            process.env.MONGO_URI,
         )
         console.log(`MongoDB connected to ${connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.log(`Error: ${error.message}`);
         process.exit(1);
     }
 }
