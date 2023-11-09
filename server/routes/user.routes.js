a45d752e1673cb105afdac9c7294d71824a026ec
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-09 21:27:15 +0530

    Add authentication middleware to user profile route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getProfile, login, logout, register } from "../controllers/user.controller.js";
+import isLoggedIn from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 
 router.post('/register',register)
 router.post('/login',login);
 router.get('/logout',logout);
-router.get('/me',getProfile);
+router.get('/me',isLoggedIn,getProfile);
 
 export default router;
