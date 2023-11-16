64e30fb48b06e700a7f970dfb4293236cfa02f36
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-16 19:27:47 +0530

    Add user subscription functionality to payment controller

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js"
+import { razorpay } from "../server.js"
 import AppError from "../utils/appError.js"
 
 
@@ -15,7 +17,41 @@ export const getRazorpayApiKey = async(req, res, next)=>{
 
 export const buySubscription = async(req, res, next)=>{
     try {
-        
+        const { id } = req.user;
+
+        const user = await User.findById(id);
+
+        if(!user){
+            return next(new AppError("User does not exist", 404))
+        }
+
+        if(user.role === "ADMIN"){
+            return next(new AppError("ADMIN cannot buy subscription", 400))
+        }
+
+        if(user.subscription.status === "active"){
+            return next(new AppError("User already has an active subscription", 400))
+        }
+
+        const subscription = await razorpay.subscriptions.create({
+            plan_id: process.env.RAZORPAY_PLAN_ID,
+            customer_notify:1,
+        });
+
+        // update user subscription
+
+        user.subscription.id = subscription.id;
+        user.status.status = "active";
+
+        await user.save();
+
+        res.status(200).json({
+            success:true,
+            message:"Subscription Successfull",
+            subscription
+        })
+
+
     } catch (error) {
         return next(new AppError(error.message, 500))
     }
