47a8cc2fa592a16474e916b044a94ae2745226a4
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-11-17 04:05:15 +0530

    Add contactUs functionality to send email upon form submission

--- server/controllers/miscellaneous.controller.js
+++ server/controllers/miscellaneous.controller.js
@@ -1,10 +1,33 @@
 import asyncHandler from '../middlewares/asyncHandler.middleware.js';
-import User from '../models/user.model.js';
 import AppError from '../utils/AppError.js';
 import sendEmail from '../utils/sendEmail.js';
 
 export const  contactUs = asyncHandler(async (req, res, next) => {
+    const { name , email, message } = req.body;
+
+    if(!name || !email || !message){
+        return next(new AppError('Name , email , Message are required',400));
+    }
     
+    try {
+        const subject = 'contact Us form';
+        const textMessage = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+
+        // await sendEmail
+        await sendEmail(
+            process.env.CONTACT_US_EMAIL,
+            subject,
+            textMessage
+        );
+    } catch (error) {
+        return next(new AppError(error.message || 'Email could not be sent',500));
+    }
+
+    res.status(200).json({
+        success:true,
+        message:'Email sent'
+    });
+
 });
 
 export const userStats = asyncHandler(async (req,res,next)=>{
