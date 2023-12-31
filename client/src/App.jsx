5bbfee19d2d38d757c159eb621bb47360fc15a02
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-01-01 01:37:04 +0530

    added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,14 @@
-import { useEffect } from "react"
-import toast from "react-hot-toast"
+
+
+import Footer from "./components/footer"
 
 const App = () => {
-  useEffect(()=>{
-    toast.success('Hello World')
-})
+
 
 
   return (
     <>
-    <h1 className=" 
-    text-4xl
-    text-center
-    text-blue-500
-    font-bold
-    ">
-      Hello World
-    </h1>
+    
     </>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +8,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 }
 
