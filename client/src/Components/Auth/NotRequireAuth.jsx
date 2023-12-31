5900fdfce8c17583eceb33c8fbe4a8f10a822df9
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2023-12-31 18:29:38 +0530

    delete rediment client part to recreate it from own using reactjs

--- client/src/Components/Auth/NotRequireAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-
-const NotRequireAuth = () => {
-  const { isLoggedIn } = useSelector((state) => state.auth);
-  return isLoggedIn ? <Navigate to={"/"} replace /> : <Outlet />;
-};
-
-export default NotRequireAuth;
