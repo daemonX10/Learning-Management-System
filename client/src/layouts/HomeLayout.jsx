e80d51d2d3615c55d91b36a9b7a1676f1f44dfd0
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-01-01 12:05:28 +0530

    Add sidebar menu to HomeLayout component

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -1,10 +1,44 @@
+import {FiMenu} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
+
 import Footer from '../components/Footer'
 
 function HomeLayout ({ children }) {
+    function changeWidth(){
+      const drawerSide = document.getElementsByClassName('drawer-side');
+      drawerSide[0].style.width = 'auto';
+    }
+
+
   return (
-    <div>
-        {children}
-        <Footer/>
+    <div className='min-h-[90vh]'>
+      <div className='drawer absolute left-0 z-50 w-full'>
+        <input type="checkbox" id='my-drawer' className='drawer-toggle' />
+        <div className='drawer-content'>
+          <label htmlFor="my-drawer">
+            <FiMenu onClick={changeWidth} size={"32px"} />
+          </label>
+        </div>
+        <div className='drawer-side w-0'>
+          <label htmlFor="my-drawer" className='drawer-overlay'></label>
+          <ul className='menu p-4 w-48 sm:w-80 bg-base-200 relative'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'> About Us</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact us</Link>
+            </li>
+            <li>
+              <Link to='/courses'>All Courses</Link>
+            </li>
+          </ul>
+
+        </div>
+
+      </div>
     </div>
   )
 }
